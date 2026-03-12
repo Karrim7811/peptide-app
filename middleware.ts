c6a79a32f555ee5c57a9a7c7b50c2c9d1e7b4b0c
Karrim7811 <karimnasser@me.com>
2026-03-12 14:32:16 -0400

    Update Next.js, fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+export function middleware() {
+  return
+}
 
-export function middleware(request: NextRequest) {
-  return NextResponse.next()
+export const config = {
+  matcher: ['/_never_match_this_route_'],
 }
